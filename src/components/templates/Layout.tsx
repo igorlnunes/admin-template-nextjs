823fbdc4680fdcbdd10fbf95df2ad2d6ec018150
Igor de Lima Nunes <igorlnunes@gmail.com>
2021-11-11 08:42:11 -0300

    Teste novo

---
+++ src/components/templates/Layout.tsx
@@ -0,0 +1,13 @@
+interface LayoutProps {
+    titulo: string
+    subtitulo: string
+    children?: any
+}
+
+export default function Layout(props: LayoutProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
